477b3cddc8fad8bc26d82701345a76b4db68a5d1
limciana <limciana@yahoo.com>
2018-01-28 19:05:01 +0800

    modified db to accept pre-seeded db

--- Android Studio Files/app/src/main/java/com/cs192/upcc/DatabaseHelper.java
+++ Android Studio Files/app/src/main/java/com/cs192/upcc/DatabaseHelper.java
@@ -10,6 +10,7 @@
 /* Code History
  * Programmer     Date     Description
  * Ciana Lim      1/27/18  Created the file to set up the database
+ * Ciana Lim      1/28/18  Modified the database to accept pre-seeded data.
  */
 
 /*
@@ -21,11 +22,13 @@
 
 package com.cs192.upcc;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
-public class DatabaseHelper extends SQLiteOpenHelper{
+public class DatabaseHelper extends SQLiteAssetHelper {
      public static final String DATABASE_NAME = "UPCC.db"; // the database name
      public static final String TABLE_1 = "subjects_table"; // the name of table 1
      public static final String TABLE_1_COL_1 = "CURRICULUM"; // the name of table 1 column 1
@@ -46,11 +49,25 @@ public class DatabaseHelper extends SQLiteOpenHelper{
      public static final String TABLE_3_COL_3 = "TOTAL_UNITS_NEEDED"; // the name of table 3 column 3
      public static final int VERSION = 3; // the version number of the database
      SQLiteDatabase sqLiteDatabase; // the SQLite database instance
+     ContentValues contentValues; // stores the data that is to be inserted to the database
+     long result; // checker to see if the data was inserted to the database
 
      public DatabaseHelper(Context context){
           super(context, DATABASE_NAME, null, VERSION);
+     }
+
+     /* Name: createDB
+      * Creation Date: 1/28/18
+      * Purpose: Instantiates the pre-seeded database and makes it available for use.
+      * Arguments: none
+      * Other Requirements:
+      *   sqLiteDatabase - SQLiteDatabase, the SQLite database instance used
+      * Return Value: void
+      */
+     public void createDB(){
           sqLiteDatabase = this.getWritableDatabase();
      }
+
 
      /*
       * Name: onCreate
@@ -61,12 +78,12 @@ public class DatabaseHelper extends SQLiteOpenHelper{
       * Other Requirements: none
       * Return Value: void
       */
-     @Override
+     /*@Override
      public void onCreate(SQLiteDatabase sqLiteDatabase){ // creates table here
           sqLiteDatabase.execSQL("create table "+TABLE_1+"("+TABLE_1_COL_1+" TEXT, "+TABLE_1_COL_2+" TEXT, "+TABLE_1_COL_3+" TEXT, "+TABLE_1_COL_4+" INT, "+TABLE_1_COL_5+" INT, "+TABLE_1_COL_6+" INT, "+TABLE_1_COL_7+" INT, "+TABLE_1_COL_8+" TEXT, "+TABLE_1_COL_9+" TEXT);");
-          sqLiteDatabase.execSQL("create table "+TABLE_2+"("+TABLE_2_COL_1+" TEXT, "+TABLE_2_COL_2+" TEXT)");
-          sqLiteDatabase.execSQL("create table "+TABLE_3+"("+TABLE_3_COL_1+" TEXT, "+TABLE_3_COL_2+" INT, "+TABLE_3_COL_3+" INT)");
-     }
+          sqLiteDatabase.execSQL("create table "+TABLE_2+"("+TABLE_2_COL_1+" TEXT, "+TABLE_2_COL_2+" TEXT);");
+          sqLiteDatabase.execSQL("create table "+TABLE_3+"("+TABLE_3_COL_1+" TEXT, "+TABLE_3_COL_2+" INT, "+TABLE_3_COL_3+" INT);");
+     }*/
 
      /*
       * Name: onUpgrade
@@ -79,11 +96,32 @@ public class DatabaseHelper extends SQLiteOpenHelper{
       * Other Requirements: none
       * Return Value: void
       */
-     @Override
-     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
-          sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_1);
-          sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_2);
-          sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_3);
+     /*@Override*/
+     /*public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
+          sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_1+";");
+          sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_2+";");
+          sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_3+";");
           onCreate(sqLiteDatabase);
-     }
+     }*/
+
+     /* public boolean insertData(String curriculum, String subject_name, String subject_description, String units, String is_JS, String is_SS, String year_to_be_taken, String prerequisites, String corequisites){
+          sqLiteDatabase = this.getWritableDatabase();
+          contentValues = new ContentValues();
+          contentValues.put(TABLE_1_COL_1, curriculum);
+          contentValues.put(TABLE_1_COL_2, subject_name);
+          contentValues.put(TABLE_1_COL_3, subject_description);
+          contentValues.put(TABLE_1_COL_4, units);
+          contentValues.put(TABLE_1_COL_5, is_JS);
+          contentValues.put(TABLE_1_COL_6, is_SS);
+          contentValues.put(TABLE_1_COL_7, year_to_be_taken);
+          contentValues.put(TABLE_1_COL_8, prerequisites);
+          contentValues.put(TABLE_1_COL_9, corequisites);
+          result = sqLiteDatabase.insert(TABLE_1, null, contentValues);
+          if(result ==  -1){
+               return false;
+          }
+          else{
+               return true;
+          }
+     } */
 }
--- Android Studio Files/app/src/main/java/com/cs192/upcc/MainActivity.java
+++ Android Studio Files/app/src/main/java/com/cs192/upcc/MainActivity.java
@@ -10,6 +10,7 @@
 /* Code History
  * Programmer     Date     Description
  * Ciana Lim      1/27/18  Set up the database for the application
+ * Ciana Lim      1/28/18  Modified database set-up to accept pre-seeded data.
  */
 
 /*
@@ -23,9 +24,14 @@ package com.cs192.upcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
      DatabaseHelper UPCCdb; // the database instance of the application
+     EditText editCurriculum, editName, editDescription, editUnits, editJS, editSS, editYear, editPrereq, editCoreq; // the fields used for inserting to the database
+     Button btnAddData; // button driver to add the data
+     boolean isInserted; // checks if the data is inserted in the database
 
      /*
       * Name: onCreate
@@ -42,5 +48,42 @@ public class MainActivity extends AppCompatActivity {
           super.onCreate(savedInstanceState);
           setContentView(R.layout.activity_main);
           UPCCdb = new DatabaseHelper(this);
+
+          editCurriculum = (EditText)findViewById(R.id.editText_curriculum);
+          editName = (EditText)findViewById(R.id.editText_name);
+          editDescription = (EditText)findViewById(R.id.editText_description);
+          editUnits = (EditText)findViewById(R.id.editText_units);
+          editJS = (EditText)findViewById(R.id.editText_js);
+          editSS = (EditText)findViewById(R.id.editText_ss);
+          editYear = (EditText)findViewById(R.id.editText_year);
+          editPrereq = (EditText)findViewById(R.id.editText_prereq);
+          editCoreq = (EditText)findViewById(R.id.editText_coreq);
+          btnAddData = (Button)findViewById(R.id.button_add);
+          UPCCdb.createDB();
      }
+
+     /* public void AddData(){
+          btnAddData.setOnClickListener(
+                  new View.OnClickListener(){
+                       @Override
+                       public void onClick(View v){
+                            isInserted = UPCCdb.insertData(editCurriculum.getText().toString(),
+                                    editName.getText().toString(),
+                                    editDescription.getText().toString(),
+                                    editUnits.getText().toString(),
+                                    editJS.getText().toString(),
+                                    editSS.getText().toString(),
+                                    editYear.getText().toString(),
+                                    editPrereq.getText().toString(),
+                                    editCoreq.getText().toString() );
+                            if(isInserted == true){
+                                 Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                 Toast.makeText(MainActivity.this, "Data not inserted", Toast.LENGTH_LONG).show();
+                            }
+                       }
+                  }
+          );
+     } */
 }
