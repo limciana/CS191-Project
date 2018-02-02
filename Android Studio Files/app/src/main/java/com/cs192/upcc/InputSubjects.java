bba0c620f3b8e112a568f4dac3c529357bc9b728
jgabaja <jgabaja@yahoo.com>
2018-02-02 15:12:42 +0800

    added files for input subject module

--- Android Studio Files/app/src/main/java/com/cs192/upcc/InputSubjects.java
+++ Android Studio Files/app/src/main/java/com/cs192/upcc/InputSubjects.java
@@ -1,3 +1,24 @@
+/*
+ * This is a course requirement for CS 192 Software Engineering II
+ * under the supervision of Asst. Prof. Ma. Rowena C. Solamo
+ * of the Department of Computer Science, College of Engineering,
+ * University of the Philippines, Diliman
+ * for the AY 2017-2018.
+ * This code is written by James Abaja.
+ */
+
+/* Code History
+ * Programmer     Date     Description
+ * James Abaja    2/2/18   Set up initial file of the back end for the Input Subjects Screen.
+ */
+
+/*
+ * File Creation Date: 2/2/18
+ * Development Group: James Abaja, Rayven Cruz, Ciana Lim
+ * Client Group: CS 192 Class
+ * Purpose of the Software: To aid the DCS students in tracking their taken subjects, and the subjects they can take afterwards.
+ */
+
 package com.cs192.upcc;
 
 import android.support.v7.app.AppCompatActivity;
