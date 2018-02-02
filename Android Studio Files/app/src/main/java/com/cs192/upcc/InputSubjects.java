8d7f3828056e5672c4978f3545c9fa623404d39a
jgabaja <jgabaja@gmail.com>
2018-02-02 12:05:29 +0800

    added logo for app icon

---
+++ Android Studio Files/app/src/main/java/com/cs192/upcc/InputSubjects.java
@@ -0,0 +1,15 @@
+package com.cs192.upcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class InputSubjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_subjects);
+    }
+}
